fcbd8e23c3939906caef82508763cfb1b828e086
AndrejGobeX <gobex7@gmail.com>
2020-12-28 23:17:14 +0100

    easier

--- ScreenRecorder.py
+++ ScreenRecorder.py
@@ -2,6 +2,13 @@ from PIL import ImageGrab
 import numpy as np
 import keyboard
 import pathlib
+import sys
+
+args = str(sys.argv)
+if len(sys.argv) == 1:
+    driver_camera = 'third_person'
+else:
+    driver_camera = 'first_person'
 
 logfile = './log.txt'
 
@@ -25,49 +32,46 @@ size = (frame_width, frame_height)
 # result = cv2.VideoWriter('video_'+str(log)+'.avi', cv2.VideoWriter_fourcc(*'MJPG'), 60, size) 
 x=0
 
-pathlib.Path('./images/0000').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/0001').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/0010').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/0011').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/0100').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/0101').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/0110').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/0111').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1000').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1001').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1010').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1011').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1100').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1101').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1110').mkdir(parents=True, exist_ok=True)
-pathlib.Path('./images/1111').mkdir(parents=True, exist_ok=True)
+image_dir = './images/' + driver_camera + '/'
 
-keyboard.wait('s')
+pathlib.Path(image_dir + '0000').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '0001').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '0010').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '0011').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '0100').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '0101').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '0110').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '0111').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1000').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1001').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1010').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1011').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1100').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1101').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1110').mkdir(parents=True, exist_ok=True)
+pathlib.Path(image_dir + '1111').mkdir(parents=True, exist_ok=True)
 
-while(True):
-    x=x+1
 
-    up = int(keyboard.is_pressed('up'))
-    down = int(keyboard.is_pressed('down'))
-    left = int(keyboard.is_pressed('left'))
-    right = int(keyboard.is_pressed('right'))
+while(not keyboard.is_pressed('q')):
+    if not keyboard.is_pressed('s'):
+        continue
 
-    image = './images/' + str(up+down*10+left*100+right*1000+10000)[1:] + '/' + str(log) + '_' + str(x) + '.jpg'
-	
-    frame = ImageGrab.grab()
-    frame.save(image)
-    print(image)
+    while(True):
+        x=x+1
 
-    # frame = np.array(frame) 
-    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        up = int(keyboard.is_pressed('up'))
+        down = int(keyboard.is_pressed('down'))
+        left = int(keyboard.is_pressed('left'))
+        right = int(keyboard.is_pressed('right'))
 
-    # result.write(frame) 
-    # cv2.imshow('Frame', frame) 
-    if(keyboard.is_pressed('f')):
-        break
+        image = image_dir + str(up+down*10+left*100+right*1000+10000)[1:] + '/' + str(log) + '_' + str(x) + '.jpg'
+        
+        frame = ImageGrab.grab()
+        frame.save(image)
+        print(image)
 
-# result.release() 
-# cv2.destroyAllWindows() 
+        if(keyboard.is_pressed('f')):
+            break
 
 file = open(logfile, 'w')
 file.write(str(log))
