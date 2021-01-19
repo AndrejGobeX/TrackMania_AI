3a2d13a4d96b6b1e6c990a4ab3a4b7c58e92bb99
AndrejGobeX <gobex7@gmail.com>
2021-01-19 19:53:28 +0100

    Maniack: fix in Mods.initial_crop()

--- Mods.py
+++ Mods.py
@@ -1,11 +1,12 @@
 # Image modification functions
 
-from PIL import Image
+from PIL import Image, ImageFilter
 import numpy as np
 import time
+import os
 
-def initial_crop(img, l, u, d, r):
-    img = img.crop((l, u, img.size[0]-d, img.size[1]-r))
+def initial_crop(img, l, u, r, d):
+    img = img.crop((l, u, img.size[0]-r, img.size[1]-d))
     return img
 
 def mod_neptune_crop(img, w, h):
@@ -21,7 +22,24 @@ def mod_maniack_crop(img, w, h):
     img = (img < 80) * 255.0
     return img
 
+def mod_road_mask_crop(img, w, h):
+    img = initial_crop(img, 300, 350, 300, 100).convert('L')
+    img = img.resize((w, h), Image.ANTIALIAS)
+    img = img.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    img = np.array(img).reshape((h, w, 1))
+    return img
 
+letter = 't'
+for name in os.listdir('./images/third_person/1001/'):
+    if name[0] == '6' or name[0] == '7':
+        img = './images/third_person/1001/' + name
+        img = Image.open(img)
+        start = time.time()
+        img = mod_road_mask_crop(img, 30, 30).reshape((30, 30))
+        end = time.time()
+        print(end - start)
+        img = Image.fromarray(img).convert('L')
+        img.save('./images/third_person/1001/'+letter+name)
 """img = Image.open('./images/third_person/0101/6_306_191.jpg')
 
 start = time.time()
